e4a0b14462deb40f76e8331a9530d92de866d168
Juan David Castro Marin <jdcastro861@soy.sena.edu.co>
2024-08-14 17:21:53 -0500

    se agregan validaciones al formulario

--- src/js/index.js
+++ src/js/index.js
@@ -14,10 +14,74 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function validarFormulario(htmlForm)
 {
-    //Si no esta el required se debe  validar el html
-    let usuario = htmlForm.usuario;
-    alert(usuario);
-    console.log(usuario);
+    //obtiene toda la etiqueta (trabaja como variable)
+    let usuario = htmlForm.usuario;    
+    if(usuario.value == '' || usuario.value != 'admin')
+    {
+        alert('El nombre es usuario no es valido');
+        usuario.focus();
+        usuario.select();
+        return false;
+    }
+    
+    let password = htmlForm.password;
+    if(password.value == '' || password.value != 'Admin123')
+    {
+        alert('contraseña no valida');
+        password.focus();
+        password.select();
+        return false;
+    }
 
+    let setTecnologias = htmlForm.tecnologia;
+    //console.log(setTecnologias);
+    if(!isAnyChecked(setTecnologias))
+    {
+        alert('No se selecciono ninguna tecnología.')
+        return false;
+    }
+
+    let setGenero = htmlForm.genero;
+    //console.log(setGenero);
+    if(!isAnyChecked(setGenero))
+    {
+        alert('No se selecciono ningún genero.');
+        return false;
+    }
+
+    let ocupacion =  htmlForm.ocupacion;
+    if(ocupacion.value === "")
+    {
+        alert("No se selecciono ninguna ocupación.")
+        ocupacion.focus();
+    }
+    //console.log(ocupacion.value);
+    let comentarios = htmlForm.comentarios;
+    console.log(comentarios);
+
+
+    //const checkboxes = document.querySelectorAll('input[name="tecnologia"]:checked');    
+    // Obtener los valores seleccionados
+    // const selectedValues = [];
+    // checkboxes.forEach((checkbox) => {
+    //   selectedValues.push(checkbox.value);
+    // });
+    
+    
+    
+}
+
+function isAnyChecked (array) 
+{
+    let isValid = false;
+    for(let i = 0; i< array.length; i++)
+    {
+        if(array[i].checked)
+        {
+            isValid = true;
+        }
+    }
+
+    return isValid;
 
 }
